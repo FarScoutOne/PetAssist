49a46fb74b43d9f7bcb507fd44c526f40e1e7c25
ryne <rsmith23@live.maryville.edu>
2024-04-19 11:27:34 -0400

    Added 3 example routes

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello</h1>"
+
+
+@app.route("/home")
+def home():
+    return "<h1>Home</h1>"
+
+
+@app.route("/json")
+def json():
+    return {"mykey": "JSON Value!", "mylist": [1, 2, 3, 4, 5]}

2cdad2dedd5f15cebf7d13ba2e22ca505dff94ab
ryne <rsmith23@live.maryville.edu>
2024-04-29 23:46:34 -0400

    Added ScheduledActivity model

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from .extensions import db
 
+
 class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -8,10 +9,12 @@ class Owner(db.Model):
 
     activities = db.relationship("Activity", back_populates="owner")
 
+
 pet_food = db.Table("pet_food",
                     db.Column("pet_id", db.ForeignKey("pet.id"), primary_key=True),
                     db.Column("food_id", db.ForeignKey("food.id"), primary_key=True)
                     )
+
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,12 +24,14 @@ class Pet(db.Model):
     activities = db.relationship("Activity", back_populates="pet")
     foods = db.relationship("Food", secondary=pet_food, back_populates="pets")
 
+
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     brand = db.Column(db.String(100))
     flavor = db.Column(db.String(100))
 
     pets = db.relationship("Pet", secondary=pet_food, back_populates="foods")
+
 
 class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,3 +43,12 @@ class Activity(db.Model):
 
     owner = db.relationship("Owner", back_populates="activities")
     pet = db.relationship("Pet", back_populates="activities")
+
+
+class ScheduledActivity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    activity_id = db.Column(db.ForeignKey("activity.id"))
+    owner_id = db.Column(db.ForeignKey("owner.id"))
+    pet_id = db.Column(db.ForeignKey("pet.id"))
+    deadline = db.Column(db.DateTime)
+    complete = db.Column(db.Boolean)
